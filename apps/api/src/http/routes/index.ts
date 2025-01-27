f33ca767b9ab3aa004a4eb06f71ae05a5524cbb3
Millena Limaverde <millenalimaverde@outlook.com>
2025-01-27 19:35:24 -0300

    feat: 🎸 plugin, authentication and password route

--- apps/api/src/http/routes/index.ts
+++ apps/api/src/http/routes/index.ts
@@ -1,9 +1,16 @@
 import { FastifyInstance } from "fastify"
 import { createAccount } from "./auth/create-account"
 import { authenticateWithPassword } from "./auth/authenticate-with-password"
+import { getProfile } from "./auth/get-profile"
+import { resetPassword } from "./auth/reset-password"
+import { requestPasswordRecover } from "./auth/request-password-recover"
+
 
 export const routeRegister = (app: FastifyInstance) => {
   app.register(createAccount)
   app.register(authenticateWithPassword)
+  app.register(getProfile)
+  app.register(requestPasswordRecover)
+  app.register(resetPassword)
 }
 
